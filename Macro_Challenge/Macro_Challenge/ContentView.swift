b4de99e090ac2e9a0a59455f2c9fe6c59ca57e44
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-05-22 14:47:49 -0300

    teste2

--- Macro_Challenge/Macro_Challenge/ContentView.swift
+++ Macro_Challenge/Macro_Challenge/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("oi")
+            Text("oi")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
