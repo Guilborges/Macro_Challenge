d1976fb6457a46254c399d8f6ac4d3c18f40afda
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-05-22 14:35:46 -0300

    Initial Project

---
+++ Macro_Challenge/Macro_Challenge/Macro_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Macro_ChallengeApp.swift
+//  Macro_Challenge
+//
+//  Created by Guilherme Borges on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Macro_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
