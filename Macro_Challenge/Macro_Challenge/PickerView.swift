a28e5b9338f2e9895ce223142e4732553dde20d0
Levenetz <victorlevenetz@gmail.com>
2023-05-29 17:39:28 -0300

    Picker view

---
+++ Macro_Challenge/Macro_Challenge/PickerView.swift
@@ -0,0 +1,43 @@
+//
+//  PickerView.swift
+//  Macro_Challenge
+//
+//  Created by Victor Levenetz Mariano on 29/05/23.
+//
+
+import Foundation
+import SwiftUI
+import _PhotosUI_SwiftUI
+
+struct SingleImagePicker: View {
+    
+    @StateObject var imagePicker = ImagePicker()
+
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                if let image = imagePicker.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else  {
+                   // let cameraPhoto = cameraPicker.sourceType
+                        
+                    Text("clicke")
+                }
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    PhotosPicker(selection: $imagePicker.imageSelection){
+                        Image(systemName: "photo")
+                            .imageScale(.large)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
