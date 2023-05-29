285232c4e986e78b082334bc8e51249b49ec5cb4
Levenetz <victorlevenetz@gmail.com>
2023-05-29 17:36:40 -0300

    Adição da classe de picker da foto

---
+++ Macro_Challenge/Macro_Challenge/PhotoPicker.swift
@@ -0,0 +1,39 @@
+//
+//  ImagePicker.swift
+//  Macro_Challenge
+//
+//  Created by Victor Levenetz Mariano on 24/05/23.
+//
+
+
+import SwiftUI
+import PhotosUI
+import Combine
+
+class ImagePicker: ObservableObject{
+    
+    @Published var image: Image?
+    @Published var imageSelection: PhotosPickerItem?{
+        
+        didSet {
+            if let imageSelection {
+                Task {
+                    try await loadTransferable(from: imageSelection)
+                }
+            }
+        }
+    }
+    
+    
+    func loadTransferable(from imageSelection: PhotosPickerItem?) async throws{
+        
+        do{
+            if let image = try await imageSelection?.loadTransferable(type: Image.self){
+                self.image = image
+            }
+        } catch {
+            print(error.localizedDescription)
+            image = nil
+        }
+    }
+}
