b08fed9c9cfb2c3082d6d886b82d7fc35a19269e
Levenetz <victorlevenetz@gmail.com>
2023-06-02 03:42:03 -0300

    Criação da vm

---
+++ Macro_Challenge/Macro_Challenge/ViewModelProduct.swift
@@ -0,0 +1,37 @@
+//
+//  ViewModelProduct.swift
+//  Macro_Challenge
+//
+//  
+//
+
+import SwiftUI
+
+struct ViewModelProduct {
+    public var productList = [ProductModel]()
+     
+    
+    public mutating func addProduct(tags: [Tag], purchasedPrice: Double, status: ProductStatus, acessory: Bool) {
+     
+        productList.append(ProductModel.init(tags: tags, purchasedPrice: purchasedPrice, status: status, acessory: acessory))
+     }
+     
+     
+     public func convertStringToDouble(text: String) -> Double {
+         let formatter = NumberFormatter()
+         formatter.numberStyle = .decimal
+         formatter.maximumFractionDigits = 2
+         
+         if let cost = Double(text) {
+             let formattedCost = String(format: "%.2f", cost)
+             return Double(formattedCost) ?? 0.0
+         } else {
+             print("Not a valid number: \(text)")
+         }
+         
+         return 0.0
+     }
+
+
+}
+
