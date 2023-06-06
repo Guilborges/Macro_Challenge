712e85d5633ebec36e69894d8e6611721e46b6c1
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-06-06 17:33:12 -0300

    TelaInicial pronta

--- Macro_Challenge/Macro_Challenge/TelaInicial.swift
+++ Macro_Challenge/Macro_Challenge/TelaInicial.swift
@@ -8,28 +8,55 @@
 import SwiftUI
 
 struct TelaInicial: View {
-    
-    @State var tags: [Tag] = []
+    @State private var tags: [Tag] = []
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Meu Brecho")
-                    .bold()
-                    .font(.largeTitle)
-                
-                Spacer()
-                
-                NavigationLink(destination: ContentView(prod: Product.init(tags: [Tag(name: "")], purchasedPrice: 200, status: ProductStatus.acquarid, acessory: true), tags: $tags )) {
-                    Image(systemName: "plus")
-                        .font(.largeTitle)
-                        .foregroundColor(.blue)
+        TabView {
+            NavigationView {
+                VStack {
+                    Text("Meu Brechó")
+                        .bold()
+                        .font(.system(size: 34, weight: .bold, design: .rounded))
+                        .padding(.vertical)
+                    Text("Total de Peças: 500")
+                    
+                    Spacer()
+                    
+                    HStack {
+                        ForEach(0..<3) { index in
+                            Rectangle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(.gray)
+                                .padding(10)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    .toolbar{
+                        ToolbarItem(placement: .navigationBarTrailing){
+                            NavigationLink(destination: ContentView(prod: Product.init(tags: [Tag(name: "")], purchasedPrice: 200, status: ProductStatus.acquarid, acessory: true), tags: $tags )) {
+                                Image(systemName: "plus")
+                            }
+                        }
+                        
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                    .padding()
                 }
-                .padding()
             }
             .navigationBarHidden(true)
+            .tabItem {
+                Label("Brechó", systemImage: "square.grid.2x2.fill")
+            }
+            
+            Text("Second Tab")
+                .tabItem {
+                    Label("Segunda", systemImage: "circle.fill")
+                }
         }
     }
+    
 }
 
 
