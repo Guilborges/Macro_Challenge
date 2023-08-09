f4c53f7d004f10896c6fc36e6486f6097034d730
Levenetz <victorlevenetz@gmail.com>
2023-08-09 14:12:24 -0300

    comits de atualizações

---
+++ Macro_Challenge/Models/ProductStatus.swift
@@ -0,0 +1,23 @@
+//
+//  ProductStatus.swift
+//  Macro_Challenge
+//
+//  Created by Victor Levenetz Mariano on 27/05/23.
+//
+
+import Foundation
+
+
+    enum ProductStatus: String, Codable{
+        
+        case washing = "Lavando"
+        case maintenance = "Manuteção"
+        case selling = "Vendendo"
+        case acquarid = "Adquirido"
+        case sold = "Vendido"
+        case nullo = "null"
+        
+    }
+    
+
+
