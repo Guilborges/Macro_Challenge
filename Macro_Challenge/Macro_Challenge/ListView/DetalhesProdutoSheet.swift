c411b880aefce16f4f8d1aad3065430c0826e870
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-08-31 17:56:46 -0300

    ajustes sheet

--- Macro_Challenge/Macro_Challenge/ListView/DetalhesProdutoSheet.swift
+++ Macro_Challenge/Macro_Challenge/ListView/DetalhesProdutoSheet.swift
@@ -8,47 +8,46 @@
 import SwiftUI
 
 struct DetalhesProdutoSheet: View {
-    @State private var exibindoDetalhes = false
     let product: Product
-    
+
     var body: some View {
         VStack {
             VStack {
-                Section{
-                    if let image = product.image{
+                Section {
+                    if let image = product.image {
                         Image(uiImage: image)
                             .resizable()
                             .scaledToFill()
                             .frame(width: 350, height: 350)
                             .clipped()
-                        
                     }
+
                     Text("R$\(String(format: "%.2f", product.purchasedPrice))")
 //                        .font(.footnote)
-                        .frame(height: 55)
                         .font(.system(size: 30, design: .rounded))
+                        .frame(height: 55)
+                        .background(Color.white)
+                        .foregroundColor(.primary)
+                        .background(Color.white)
                 }
-                
-                
             }
             .border(Color.black, width: 0.5)
-            .foregroundColor(.primary)
-            .background(Color.white)
-            .cornerRadius(20)
-            
-    //            Text("Nome: \(product.tags)")
+            .cornerRadius(15)
+
+//            Text("Nome: \(product.tags)")
             Text("Preço: \(product.status.rawValue)")
-            
-            }
-            .padding(300)
-            .background(Color("background"))
-       
+                .foregroundColor(.primary)
+
+            Spacer()
         }
-    
+        .padding()
+        .background(Color("background"))
     }
-    extension Array {
-        subscript(safe index: Index) -> Element? {
-            return indices.contains(index) ? self[index] : nil
-        }
+}
+
+extension Array {
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
+}
 
